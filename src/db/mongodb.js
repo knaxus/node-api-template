ac184c0c0138415d1a183aea5b43994f77a0998c
Ashok Dey <ashokdey100@gmail.com>
2019-03-20 07:47:37 +0530

    --update: handle missing ENV var

--- src/db/mongodb.js
+++ src/db/mongodb.js
@@ -5,12 +5,16 @@ mongoose.Promise = global.Promise;
 
 // const MONGODB_URI = `mongodb://${username}:${password}@${host}:${port}/${databaseName}`;
 
-// console.log(MONGODB_URI);
 const options = {
   reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
   reconnectInterval: 500, // Reconnect every 500ms
   useNewUrlParser: true,
 };
+
+if (!process.env.MONGODB_URI) {
+  logger.error('Please set MONGO_URI');
+  process.exit(-1);
+}
 
 mongoose.connect(process.env.MONGODB_URI, options);
 
@@ -23,17 +27,15 @@ mongoose.connect(process.env.MONGODB_URI, options);
 // });
 
 mongoose.connection.on('connected', () => {
-  // eslint-disable-next-line no-console
   logger.info('Connected to MongoDB');
 });
+
 mongoose.connection.on('error', (err) => {
-  // eslint-disable-next-line no-console
   logger.error('MongoDB connection error:', err);
   process.exit(-1);
 });
 
 mongoose.connection.on('disconnected', () => {
-  // eslint-disable-next-line no-console
   logger.error('MongoDB disconnected');
 });
 
