63eb4e2d500bb727299fdaf94084a586de4cbdd8
Ashok Dey <ashokdey100@gmail.com>
2019-04-03 15:42:28 +0530

    --fix: handle both custom and error messages

--- src/utils/handleCustomErrors.js
+++ src/utils/handleCustomErrors.js
@@ -7,16 +7,16 @@ function handleCustomThrow(res, error) {
     return sendResponse(res, 409, {}, 'Duplicate entry');
   }
   if (error.code === 400) {
-    return sendResponse(res, error.code, {}, error.msg);
+    return sendResponse(res, error.code, {}, error.msg || error.message);
   }
   if (error.code === 401) {
-    return sendResponse(res, error.code, {}, error.msg);
+    return sendResponse(res, error.code, {}, error.msg || error.message);
   }
   if (error.code === 403) {
-    return sendResponse(res, error.code, {}, error.msg);
+    return sendResponse(res, error.code, {}, error.msg || error.message);
   }
   if (error.code === 404) {
-    return sendResponse(res, error.code, {}, error.msg);
+    return sendResponse(res, error.code, {}, error.msg || error.message);
   }
   return sendResponse(res, 500, {}, 'Something went wrong');
 }
